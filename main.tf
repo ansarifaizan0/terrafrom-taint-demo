8644c774ecba9f5a297141c919d49259e85fa09c
Faizan <ansari.faizan0@gmail.com>
2026-02-24 15:58:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# 👇 auto fetch latest Amazon Linux 2 AMI for your region
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = data.aws_ami.amazon_linux.id   # 👈 dynamic, always correct
   instance_type = "t2.micro"
 
   tags = {
